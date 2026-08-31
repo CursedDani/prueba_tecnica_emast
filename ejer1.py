69534f18bbc8d5d6a453998802eeb97317f3a1c0
CursedDani <dfarangog@gmail.com>
2026-08-31 13:51:24 -0500

    Correcion: Mas ejemplos para cada ejercicio logico

--- ejer1.py
+++ ejer1.py
@@ -13,7 +13,7 @@ def Solitary(arr:list) -> int:
         if value:
             return key
 
-#Logica de datos 2:
+# Logica de datos 2:
 # Limpieza de ceros
 
 def ZeroCleaner(arr:list) -> list:
@@ -27,6 +27,13 @@ def ZeroCleaner(arr:list) -> list:
     count.extend([0] * z_count)
     return count
 
+# Logica de datos 3:
+# Compresor de texto
 
-print(f'Ejercicio 1 Ejemplo output :{Solitary([4, 1, 2, 1, 2])}')
-print(f'Ejercicio 2 Ejemplo output :{ZeroCleaner([0, 1, 0, 3, 12] )}')
+
+print(f'Ejercicio 1 Ejemplo 1 output : {Solitary([4, 1, 2, 1, 2])}')
+print(f'Ejercicio 1 Ejemplo 2 output : {Solitary([2,2,1,1,3,3,4,4,5,5,10,10,12,12])}')
+print(f'Ejercicio 1 Ejemplo 3 output : {Solitary([0,0,0,0,0,0,1,0,0,0,0])}')
+print(f'Ejercicio 2 Ejemplo 1 output : {ZeroCleaner([0, 1, 0, 3, 12] )}')
+print(f'Ejercicio 2 Ejemplo 2 output : {ZeroCleaner([0, 0, 1] )}')
+print(f'Ejercicio 2 Ejemplo 3 output : {ZeroCleaner([0, 0, 0, 0, 0] )}')
