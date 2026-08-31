57fb18e0efb840ae8321073c47ee389ff828371b
CursedDani <dfarangog@gmail.com>
2026-08-31 13:54:49 -0500

    Ejercicio 3 Logica

--- ejer1.py
+++ ejer1.py
@@ -30,6 +30,18 @@ def ZeroCleaner(arr:list) -> list:
 # Logica de datos 3:
 # Compresor de texto
 
+def TextCompressor(text:str) -> str:
+    count = {}
+    for i in text:
+        if i in count:
+            count[i] += 1
+        else:
+            count[i] = 1
+
+    compressed = ''
+    for key, value in count.items():
+        compressed += f'{key}{value}'
+    return compressed
 
 print(f'Ejercicio 1 Ejemplo 1 output : {Solitary([4, 1, 2, 1, 2])}')
 print(f'Ejercicio 1 Ejemplo 2 output : {Solitary([2,2,1,1,3,3,4,4,5,5,10,10,12,12])}')
@@ -37,3 +49,6 @@ print(f'Ejercicio 1 Ejemplo 3 output : {Solitary([0,0,0,0,0,0,1,0,0,0,0])}')
 print(f'Ejercicio 2 Ejemplo 1 output : {ZeroCleaner([0, 1, 0, 3, 12] )}')
 print(f'Ejercicio 2 Ejemplo 2 output : {ZeroCleaner([0, 0, 1] )}')
 print(f'Ejercicio 2 Ejemplo 3 output : {ZeroCleaner([0, 0, 0, 0, 0] )}')
+print(f'Ejercicio 3 Ejemplo 1 output : {TextCompressor("aaabbbcc")}')
+print(f'Ejercicio 3 Ejemplo 2 output : {TextCompressor("abc")}')
+print(f'Ejercicio 3 Ejemplo 3 output : {TextCompressor("aabbaa")}')
